bdc9c6df59f60528863f9cb986777d2287d8e999
TalhaQtech <muhammadtalha@qtechnologies.org>
2022-06-29 11:23:23 -0700

    Booking Details  Completed

---
+++ lib/screens/booking/booking.dart
@@ -0,0 +1,214 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:stande_aero/helper/colors.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class booking extends StatelessWidget {
+  booking({Key? key}) : super(key: key);
+  double pad = 23.0;
+  @override
+  Widget build(BuildContext context) {
+    double res_width = MediaQuery.of(context).size.width;
+    double res_height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        leading: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            Navigator.pop(context);
+            // _key.currentState!.openDrawer();
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Container(
+                width: 25,
+                child: Image.asset(
+                  'assets/slicing/Untitled-3.png',
+                )),
+          ),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'CF34-10 DAE',
+              style: TextStyle(color: Colors.black),
+            ),
+            Container(
+                width: 40,
+                height: 40,
+                child: Image.asset('assets/slicing/Untitled-4.png',
+                    fit: BoxFit.cover)),
+          ],
+        ),
+        // actions: [
+        //   Padding(
+        //     padding: const EdgeInsets.all(8.0),
+        //     child: GestureDetector(
+        //       onTap: () {
+        //         Get.to(Editprofile());
+        //       },
+        //       child: Container(
+        //           width: 30,
+        //           height: 20,
+        //           child: Image.asset('assets/slicing/Untitled-45.png',
+        //               fit: BoxFit.contain)),
+        //     ),
+        //   ),
+        // ],
+      ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        width: double.infinity,
+        // height:  double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/slicing/Untitled-46.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              custum_Slider(res_width: res_width),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: pad, vertical: pad / 2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'CF34-10 DAE',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      "Location: Miami, Florida",
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    ),
+                    // Text(
+                    //   'Miami, Florida',
+                    //   style: TextStyle(color: Colors.black),
+                    // ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: pad),
+                child: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing",
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(pad),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: TableCalendar(
+                    // daysOfWeekVisible: false,
+                    
+                    calendarStyle: CalendarStyle(
+                        todayDecoration: BoxDecoration(
+                            color: kPrimaryColor,
+                            borderRadius: BorderRadius.circular(50)),
+                        selectedDecoration:
+                            BoxDecoration(color: kPrimaryColor)),
+                    // calendarFormat: false,
+                    headerStyle: HeaderStyle(
+                        leftChevronIcon: CircleAvatar(
+                          backgroundColor: kPrimaryColor,
+                          child: Icon(
+                            Icons.chevron_left,
+                            color: Colors.white,
+                            size: 28,
+                          ),
+                        ),
+                        rightChevronIcon: CircleAvatar(
+                          backgroundColor: kPrimaryColor,
+                          child: Icon(
+                            Icons.chevron_right,
+                            color: Colors.white,
+                            size: 28,
+                          ),
+                        ),
+                        decoration: BoxDecoration(color: Colors.white)),
+                    firstDay: DateTime.utc(2010, 10, 16),
+                    lastDay: DateTime.utc(2030, 3, 14),
+                    focusedDay: DateTime.now(),
+                  ),
+                ),
+              ),
+              Container(
+                width: res_width * 0.9,
+                decoration: BoxDecoration(
+                    color: Color(0xffaf8a39),
+                    borderRadius: BorderRadius.all(Radius.circular(7))),
+                child: Padding(
+                  padding: EdgeInsets.all(13.0),
+                  child: Center(
+                    child: Text(
+                      'Select Booking Dates',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class custum_Slider extends StatelessWidget {
+  const custum_Slider({
+    Key? key,
+    required this.res_width,
+  }) : super(key: key);
+
+  final double res_width;
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 190.0,
+        autoPlay: true,
+        enlargeCenterPage: true,
+        viewportFraction: 0.9,
+        aspectRatio: 7.0,
+        initialPage: 2,
+      ),
+      items: [
+        1,
+        2,
+        3,
+      ].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Card(
+              elevation: 8,
+              child: Image.asset(
+                "assets/slicing/Untitled-26.png",
+                width: res_width,
+                fit: BoxFit.fill,
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:stande_aero/screens/Profile/profile.dart';
+import 'package:stande_aero/screens/booking/booking.dart';
 import 'package:stande_aero/screens/home/drawer.dart';
 // import 'package:stande_aero/screens/home/Profile/editprofile.dart';
 // import 'package:stande_aero/screens/home/Profile/profile.dart';
@@ -14,7 +15,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     double res_width = MediaQuery.of(context).size.width;
     double res_height = MediaQuery.of(context).size.height;
-  final GlobalKey<ScaffoldState> _key = GlobalKey();
+    final GlobalKey<ScaffoldState> _key = GlobalKey();
 
     return Container(
       width: double.infinity,
@@ -25,16 +26,14 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       child: Scaffold(
-             key: _key,
-      drawer: NavDrawer(),
+        key: _key,
+        drawer: NavDrawer(),
         backgroundColor: Colors.transparent,
         appBar: AppBar(
-          
           automaticallyImplyLeading: false,
           backgroundColor: Colors.transparent,
           elevation: 0,
           centerTitle: true,
-          
           leading: GestureDetector(
             behavior: HitTestBehavior.translucent,
             onTap: () {
@@ -160,34 +159,39 @@ class HomeScreen extends StatelessWidget {
   Widget StandsBox(context, image) {
     double res_width = MediaQuery.of(context).size.width;
     double res_height = MediaQuery.of(context).size.height;
-    return Container(
-      width: res_width * 0.475,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(child: Image.asset(image, fit: BoxFit.cover)),
-          SizedBox(
-            height: res_height * 0.01,
-          ),
-          Text(
-            'CF34-10 DAE',
-            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
-          ),
-          SizedBox(
-            height: res_height * 0.0015,
-          ),
-          Text(
-            'Location: Miami, Florida',
-            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
-          ),
-          SizedBox(
-            height: res_height * 0.0015,
-          ),
-          Text(
-            'Lorem ipsum dolor sit amet, con-sectetur adip',
-            style: TextStyle(fontSize: 13),
-          )
-        ],
+    return GestureDetector(
+      onTap: () {
+        Get.to(booking());
+      },
+      child: Container(
+        width: res_width * 0.475,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(child: Image.asset(image, fit: BoxFit.cover)),
+            SizedBox(
+              height: res_height * 0.01,
+            ),
+            Text(
+              'CF34-10 DAE',
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+            ),
+            SizedBox(
+              height: res_height * 0.0015,
+            ),
+            Text(
+              'Location: Miami, Florida',
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+            ),
+            SizedBox(
+              height: res_height * 0.0015,
+            ),
+            Text(
+              'Lorem ipsum dolor sit amet, con-sectetur adip',
+              style: TextStyle(fontSize: 13),
+            )
+          ],
+        ),
       ),
     );
   }
